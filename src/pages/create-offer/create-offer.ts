35a19f8bf83fef9681f21fc47f7c2dfadd525583
anton <antonkats@gmail.com>
2017-03-21 20:36:01 -0400

    minor additions to create offer page

--- src/pages/create-offer/create-offer.ts
+++ src/pages/create-offer/create-offer.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import {NavController, NavParams, ToastController} from 'ionic-angular';
 import md5 from "md5"
+import {ServerComms} from "../../providers/server-comms";
 
 /*
   Generated class for the CreateOffer page.
@@ -16,17 +17,27 @@ export class CreateOfferPage {
   user_id = "";
   display_name = "";
   hours = 0.5;
-  with_what = "";
+  description = "";
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public toastCtrl: ToastController,
+              public comms: ServerComms) {
     console.log("nav params", navParams.data)
-    this.user_id = navParams.get('user_id')
-    this.display_name = navParams.get('display_name')
+    this.user_id = navParams.get('user_id') || ""
+    this.display_name = navParams.get('display_name') || ""
   }
 
   offerHelp() {
     console.log("offering help")
-    console.log(this.hours, this.with_what)
+    console.log(this.user_id, this.hours, this.description)
+
+    let request = {'to_user_id': this.user_id, 'hours': this.hours, 'description': this.description}
+    this.comms.sendToServer("/offers/create", request, data => {
+      let r = data["response"]
+      console.log("on offer creation response was", r)
+    }, error => {
+      console.log("error during a connections lookup", error);
+      ServerComms.errorToast(this.toastCtrl, error["error_msg"])
+    })
   }
 
   increaseHours() {
