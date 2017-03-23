cb27a9facbe19066e2aea7d2091aa360023c743c
anton <antonkats@gmail.com>
2017-03-23 11:52:04 -0400

    offers can be rejected and completed

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component} from '@angular/core';
 import md5 from "md5"
-import {NavController, ToastController} from 'ionic-angular';
+import {NavController, ToastController, ModalController, AlertController} from 'ionic-angular';
 import {ConnectionsPage} from "../connections/connections";
 import {CreateOfferPage} from "../create-offer/create-offer";
 import {ServerComms} from "../../providers/server-comms";
@@ -13,11 +13,12 @@ import {ServerComms} from "../../providers/server-comms";
 export class HomePage {
   // root_page = HomePage;
   offers = [];
-  display_names = []
+  display_names = {}
 
   connectionsPage;
 
-  constructor(public navCtrl: NavController, public comms: ServerComms, public toastCtrl: ToastController) {
+  constructor(public navCtrl: NavController, public comms: ServerComms, public toastCtrl: ToastController,
+              private alertCtrl: AlertController) {
     this.connectionsPage = ConnectionsPage
 
     this.comms.sendToServer("/offers/get", null, data => {
@@ -28,10 +29,8 @@ export class HomePage {
       // now resolving display names
       let ids = this.offers.map(o => o["from_user_id"])
       this.comms.sendToServer("/connections/resolve-to-name", ids, data => {
-        this.display_names = data["response"]
+        data["response"].map(n => this.display_names[n[0]] = n[1])
         console.log("display names resolved", this.display_names)
-
-        this.offers.map((o, i) => o["display_name"] = this.display_names[i])
 
       }, error => {
         console.log("error during retrieving offers", error);
@@ -46,14 +45,105 @@ export class HomePage {
     // this.navCtrl.push(CreateOfferPage)
   }
 
+  completeOffer(id) {
+    let payload = {'offer_id': id}
+    let hours = this.offers.filter((o) => o["offer_id"] == id)[0]["hours"]
+    let prompt_text = "you want to accept " + this.displayAmount(hours)
+    let cthis = this
+    let callback = function() {
+      cthis.comms.sendToServer("/offers/complete", payload, data => {
+        let r = data["response"]
+        if (r["transaction_id"]) {
+          cthis.offers = cthis.offers.filter(o => o["offer_id"] != r['offer_id'])
+
+          let msg = "successfully transferred " + cthis.displayAmount(r['amount']) + " from '" + r["from_user_id"] + "'"
+          let toast = cthis.toastCtrl.create({
+            message: msg,
+            position: 'bottom',
+            cssClass: 'success-toast',
+            duration: 3000
+          });
+          toast.present()
+        } else {
+          ServerComms.errorToast(cthis.toastCtrl, "we failed to complete this transaction")
+        }
+      }, error => {
+        console.log("error while accepting an offer with id", id, error);
+        ServerComms.errorToast(cthis.toastCtrl, error["error_msg"])
+      })
+    }
+
+    this.actOnOffer(prompt_text, callback)
+  }
+
+  rejectOffer(id) {
+    let payload = {'offer_id': id}
+    let user_id = this.offers.find(o => o['offer_id'] == id)["from_user_id"]
+    let display_name = this.display_names[user_id]
+    let prompt_text = "you are rejecting offer from '" + display_name + "'"
+    let cthis = this
+    let callback = function() {
+      cthis.comms.sendToServer("/offers/reject", payload, data => {
+        let r = data["response"]
+        if (r) {
+          cthis.offers = cthis.offers.filter(o => o["offer_id"] != id)
+
+          let msg = "offer from '" + display_name + "' is rejected"
+          let toast = cthis.toastCtrl.create({
+            message: msg,
+            position: 'bottom',
+            cssClass: 'success-toast',
+            duration: 3000
+          });
+          toast.present()
+        } else {
+          ServerComms.errorToast(cthis.toastCtrl, "we failed to reject this offer")
+        }
+      }, error => {
+        console.log("error while rejecting an offer with id", id, error);
+        ServerComms.errorToast(cthis.toastCtrl, error["error_msg"])
+      })
+    }
+
+    this.actOnOffer(prompt_text, callback)
+  }
+
+  actOnOffer(text, callback) {
+    let alert = this.alertCtrl.create({
+      title: 'Confirm',
+      message: text,
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('offer action was NOT completed');
+          }
+        },
+        {
+          text: 'Confirm',
+          handler: () => {
+            console.log('offer action was completed');
+            callback()
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
+  offerHelp() {
+    this.navCtrl.push(this.connectionsPage)
+  }
+
   md5(what: any) {
     return md5(what)
   }
 
   displayAmount(amount: number) {
-    var suffix = "hour"
-    if (amount > 1) {
-      suffix = "hours"
+    var suffix = "hours"
+    if (amount == 1) {
+      suffix = "hour"
     }
     return amount + " " + suffix
   }
